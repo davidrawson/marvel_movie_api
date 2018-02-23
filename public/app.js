077dbebfae06c7e3f09dc98ccfca8c09557e3405
Mark Blanford <markytorture@hotmail.com>
2018-02-23 17:32:44 +0000

    API call successful, movie list object literal added and used to populate a dropdown

--- public/app.js
+++ public/app.js
@@ -3,10 +3,21 @@ const app = function(){
   let movieId = "tt1825683";
 
   const url = " http://www.omdbapi.com/?i=" + movieId + "&apikey=" + API_KEY;
-
+  populateDropdown();
   makeRequest(url, requestComplete);
 
 }
+//Populate movie picker dropdown
+const populateDropdown = function(){
+  const movieSelector = document.getElementById('movie-picker');
+  movies.forEach(function(movie){
+    const movieOption = document.createElement('option');
+    movieOption.innerText = movie.name;
+    movieOption.value = movie.id;
+    movieSelector.appendChild(movieOption);
+  })
+}
+
 
 //API access:
 const makeRequest = function(url, callback){
@@ -29,6 +40,7 @@ const requestComplete = function(){
 
 
 //Movie picker:
+//Use movieNames to populate programmatically
 
 //movieId = result of whichever option is picked
 
---
+++ public/movienames.js
@@ -0,0 +1,21 @@
+const movies = [
+{name: "Iron Man (2008)", id: "tt0371746"},
+{name: "The Incredible Hulk (2008)", id: "tt0800080"},
+{name: "Iron Man 2 (2010)", id: "tt1228705"},
+{name: "Thor (2011)", id: "tt0800369"},
+{name: "Captain America: The First Avenger (2011)", id: "tt0458339"},
+{name: "Avengers Assemble (2012)", id: "tt0848228"},
+{name: "Iron Man 3 (2013)", id: "tt1300854"},
+{name: "Thor: The Dark World (2013)", id: "tt1981115"},
+{name: "Captain America: The Winter Soldier (2014)", id: "tt1843866"},
+{name: "Guardians of the Galaxy (2014)", id: "tt2015381"},
+{name: "Avengers: Age of Ultron (2015)", id: "tt2395427"},
+{name: "Ant-Man (2015)", id: "tt0478970"},
+{name: "Captain America: Civil War (2016)", id: "tt3498820"},
+{name: "Doctor Strange (2016)", id: "tt1211837"},
+{name: "Guardians of the Galaxy Vol. 2 (2017)", id: "tt3896198"},
+{name: "Spider-Man: Homecoming (2017)", id: "tt2250912"},
+{name: "Thor: Ragnarok (2017)",id: "tt3501632"},
+{name: "Black Panther (2018)", id: "tt1825683"}
+
+]
