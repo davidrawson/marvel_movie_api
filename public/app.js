e3cd2672592c52e83787678fe27468808238ee3a
Mark Blanford <markytorture@hotmail.com>
2018-02-23 18:45:18 +0000

    dropdown change stores movie id in local storage and reloads the page, resetting the api url for that id

--- public/app.js
+++ public/app.js
@@ -1,21 +1,39 @@
 const app = function(){
 
-  let movieId = "tt1825683";
+  const lastMovieId = localStorage.getItem('last movie')
 
+  let movieId = null;
+
+  if(lastMovieId === null){
+    movieId = "tt1825683";
+  } else {
+    movieId = lastMovieId;
+  }
   const url = " http://www.omdbapi.com/?i=" + movieId + "&apikey=" + API_KEY;
-  populateDropdown();
+
   makeRequest(url, requestComplete);
 
-}
-//Populate movie picker dropdown
-const populateDropdown = function(){
   const movieSelector = document.getElementById('movie-picker');
+  movieSelector.addEventListener('change', pickMovie);
+  populateDropdown(movieSelector);
+
+
+
+}
+//Movie picker dropdown
+const populateDropdown = function(parent){
   movies.forEach(function(movie){
     const movieOption = document.createElement('option');
     movieOption.innerText = movie.name;
     movieOption.value = movie.id;
-    movieSelector.appendChild(movieOption);
+    parent.appendChild(movieOption);
   })
+}
+
+const pickMovie = function(){
+  movieId = this.value;
+  localStorage.setItem('last movie', this.value)
+  // document.location.reload(false);
 }
 
 
